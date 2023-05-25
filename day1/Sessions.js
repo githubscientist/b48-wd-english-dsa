b61fcd6cf8af9e220118a52effe6f77ab1eff657
githubscientist <sathish.datascientist@gmail.com>
2023-05-25 09:26:15 +0530

    day 1

---
+++ day1/Sessions.js
@@ -0,0 +1,33 @@
+/*
+    Sessions: 5
+
+    Session: 1
+        - How to get into Product based companies
+        - How to prepare for the coding interviews? platforms/resources
+        - An example problem - how should you approach and solve in an interview
+        - What is that the interviewer might expect in an interview
+
+    Session: 2
+        - How to calculate time complexity of a code/function?
+        - Asymptotic Notations
+        - Big - 'O' notation
+        - Examples - 8 - 10
+
+    Session: 3
+        - Linked Lists - DS
+        - Types
+        - Arrays Vs Linked Lists
+        - Implementation of Linked List
+        - Example interview problems from Linked Lists
+
+    Session: 4
+        - Stacks
+        - Implementation of Stack - Array & Object Implementations
+        - Example interview problems from Stack
+    
+    Session: 5
+        - Recursion
+        - Recursion Tree, Time Complexity
+        - Recurrence relation, backward substitution
+
+*/
---
+++ day1/platforms.js
@@ -0,0 +1,18 @@
+/*
+    Competitive Programming + DSA practice platforms:
+
+    Guvi - CodeKata
+    leetcode.com
+    interviewbit.com
+    hackerrank.com
+    hackerearth.com
+    codechef.com
+    codeforces.com
+    projecteuler.net
+    spoj.com
+    beecrowd.com.br
+
+    Book Resource:
+
+    Cracking the Coding Interview by Gayle Laakman McDowell
+*/
